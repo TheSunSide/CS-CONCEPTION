f742022edb3af355c94173f97f52416ced9e51a5
TheSunSide <60361981+TheSunSide@users.noreply.github.com>
2024-01-11 19:13:20 -0500

    Fixing make

--- debug/src/swing.hpp
+++ debug/src/swing.hpp
@@ -1,19 +1,23 @@
 #ifndef SWING_H
 #define SWING_H
-class Swing {
-    public:
-        enum Type {
-            kid,
-            school
-        };
-        void update(int = 1);
-        float calculateHeight();
-        Swing(int id, Type type, float rope);
-    private:
-        Type _type;
-        float _ropeLength;
-        int _id;
-        int _horizontalPos;
-        int _direction; //TODO set to 1
+class Swing
+{
+public:
+    enum Type
+    {
+        kid,
+        school
+    };
+    void update(int = 1);
+    float calculateHeight();
+    Swing(int id, Type type, float rope);
+    ~Swing() = default;
+
+private:
+    Type _type;
+    float _ropeLength;
+    int _id;
+    int _horizontalPos;
+    int _direction; // TODO set to 1
 };
 #endif
