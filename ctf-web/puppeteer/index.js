9c0ac2fe9ee40f768eb4931554a9d7cd0c903a45
TheSunSide <60361981+TheSunSide@users.noreply.github.com>
2024-01-28 02:48:21 -0500

    Fix puppet for server

--- ctf-web/puppeteer/index.js
+++ ctf-web/puppeteer/index.js
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 
 (async () => {
   // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({ headless: "new", executablePath: "/usr/bin/chromium-browser" });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
